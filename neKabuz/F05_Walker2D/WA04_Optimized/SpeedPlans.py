a09ebd0eff94268fff099c04da4525c217f5aaf4
IkerLenena <145427343+IkerLenenaMA@users.noreply.github.com>
2025-11-21 16:58:41 +0100

    Hexapod Innit
    Hexapod Innit

--- neKabuz/F05_Walker2D/WA04_Optimized/SpeedPlans.py
+++ neKabuz/F05_Walker2D/WA04_Optimized/SpeedPlans.py
@@ -3,14 +3,14 @@ import numpy as np
 TRAINING_PLANS = [
     # Abiadura bakarra
     np.array([-1.0], dtype = np.float32),
-	np.array([-0.5], dtype = np.float32),
-	np.array([0.0], dtype = np.float32),
-	np.array([0.5], dtype = np.float32),
-	np.array([1.0], dtype = np.float32),
-	np.array([1.5], dtype = np.float32),
-	np.array([2.0], dtype = np.float32),
-	np.array([2.5], dtype = np.float32),
-	np.array([3.0], dtype = np.float32),
+    np.array([-0.5], dtype = np.float32),
+    np.array([0.0], dtype = np.float32),
+    np.array([0.5], dtype = np.float32),
+    np.array([1.0], dtype = np.float32),
+    np.array([1.5], dtype = np.float32),
+    np.array([2.0], dtype = np.float32),
+    np.array([2.5], dtype = np.float32),
+    np.array([3.0], dtype = np.float32),
 
     # Ibili geatu ibili
     np.array([-1.0, -1.0, 0.0, 0.0, -1.0, -1.0], dtype = np.float32),
